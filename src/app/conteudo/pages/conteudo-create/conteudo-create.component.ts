fa6ece06fc42ad8f4227fb23ca07127aa740738c
EllenBecker <ellenbeckerdc@gmail.com>
2022-12-10 11:55:05 -0300

    Metodos para cadastro de um conteudo

--- src/app/conteudo/pages/conteudo-create/conteudo-create.component.ts
+++ src/app/conteudo/pages/conteudo-create/conteudo-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { IConteudo } from '../../models/IConteudo.model';
+import { ConteudoService } from '../../services/conteudo.service';
 
 @Component({
   selector: 'conteudo-create',
@@ -9,11 +11,23 @@ import { IConteudo } from '../../models/IConteudo.model';
 export class CreateConteudoComponent implements OnInit {
   conteudo = {} as IConteudo;
 
-  constructor() {}
+  constructor(
+    private conteudoService: ConteudoService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
   salvarConteudo() {
-    console.log('salvarConteudo');
+    this.conteudo = {
+      tipoTecnologia: 'NODEJS',
+      descricao: 'Descrição teste de cadastro',
+    } as IConteudo; // ToDo: utilizado ate a criação dos campos em tela
+    this.conteudoService.cadastrar(this.conteudo).then((result) => {
+      console.log(`Conteudo cadastrado com sucesso. ID: ${result?.id}`);
+      this.conteudo = {} as IConteudo;
+
+      this.router.navigate(['conteudo/list']);
+    });
   }
 }
--- src/app/conteudo/services/conteudo.service.ts
+++ src/app/conteudo/services/conteudo.service.ts
@@ -1,8 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IConteudo } from '../models/IConteudo.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ConteudoService {
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
+
+  cadastrar(conteudo: IConteudo): Promise<IConteudo | undefined> {
+    return this.httpClient
+      .post<IConteudo>('http://localhost:3000/conteudo/', conteudo)
+      .toPromise();
+  }
 }
